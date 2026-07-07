08a1efce8b02a8bd1d74a55f5cc77e21510ba1ba
Sienna White <siennarwhite@gmail.com>
2026-07-07 16:02:55 -0700

    looking reasonable so far

--- run_floc_model_enkf1.jl
+++ run_floc_model_enkf1.jl
@@ -53,22 +53,11 @@ end
 
 function run_my_model(file_out_name::String, floc_on::Bool=true)
 
-    df = CSV.read("/global/homes/s/siennaw/scratch/siennaw/scripts/enkf_sediment/adv_shear_4_model.csv", DataFrame)
-    time_steps = df[!, "seconds"]
-    turbulent_shears = df[!, "smoothed_shear"] #./10
-
-    function get_shear(tstep::Int)
-        # divide by 10 
-        index = div(tstep, 2) + 1
-        return turbulent_shears[index]
-    end 
-
     #********************** SPATIAL DOMAIN  ***************************
     N = 50 #250    # number of ensembles points
     dt = 0.5    # (seconds) size of time step 
     M  = 3600*2*25*5  #24*5 #3600*5 #3600
 
-    interval = 2000 # M*2 #60*5//dt 
 
     # Increments for saving profiles. set to 1 to save all; 10 saves every 10th, etc. 
     isave = 60*5*10
@@ -90,6 +79,7 @@ function run_my_model(file_out_name::String, floc_on::Bool=true)
     ssc_init = ssc0[1, :]   # Initial sediment concentration for each size class
     D = logrange(1e-6, 1500e-6, Ns)      # Sediment grain sizes (\mu m )
     D = collect(D)
+    Volumes = (4/3)*pi*(D./2).^3
 
     Alphas = create_lognormal_distribution(0.35, 0.3^2, N)
     Betas = create_lognormal_distribution(0.045, 0.05^2, N) #(0.055, 0.05^2, N)
@@ -102,33 +92,107 @@ function run_my_model(file_out_name::String, floc_on::Bool=true)
 
     add_sediment_to_output(Ns, isave, M, N)
 
+
+    #***************************************************************************
+    #   OBSERVATIONAL DATA 
+    #*************************************************************************** 
+    @info "Reading observational data..."
+
+    # Shear data
+    df = CSV.read("/global/homes/s/siennaw/scratch/siennaw/scripts/enkf_sediment/adv_shear_4_model.csv", DataFrame)
+    time_steps = df[!, "seconds"]
+    turbulent_shears = df[!, "smoothed_shear"] #./10
+
+    @assert dt<=1 "Head's up: Time step dt must be less than 1 second for this shear data to work properly."
+
+    # Build function to read in
+    function get_shear(tstep::Int)
+        # divide by 10 
+        int_ = Int(div(1, dt))
+        index = div(tstep, int_) + 1
+        return turbulent_shears[index]
+    end 
+
+    # LISST data
+    Ds_LISST = [  1.21,   1.6 ,   1.89,  2.23 ,  2.63,  3.11,  3.67,   4.33,   5.11 ,  6.03,
+                  7.11,   8.39,   9.9,   11.7,   13.8,  16.3,  19.2,   22.7,   26.7,   31.6,
+                  37.2,   43.9,   51.9,  61.2,   72.2,  85.2,  101.,   119.,   140.,   165.,
+                  195.,   230.,   273.,  324.,   386.,  459.]
+    N_lisst = length(Ds_LISST)
+
+    # Initial observation operator matrix H (N_lisst x Ns)
+    Ns_aug = Ns + 4 
+    H = zeros((N_lisst, Ns_aug))
+    
+    # Create volume-weighted observation operator matrix H
+    for j in 1:Ns
+        @debug "Sorting floc size class: ", D[j]*1e6
+        for i in 1:N_lisst
+            if ((D[j]*1e6) <= Ds_LISST[i])
+                @debug "-> placing in LISST size class: ", Ds_LISST[i]
+                H[i, j] = Volumes[j]
+                break
+            end 
+            if i==N_lisst
+                @debug "Larger than max(LISST) -> placing in LISST size class: ", Ds_LISST[i]
+                H[i, j] = Volumes[j]
+                break
+            end 
+        end
+    end 
+
+    # Volume-weighted observations 
+    dfL = CSV.read("/global/homes/s/siennaw/scratch/siennaw/scripts/enkf_sediment/lisst_data.csv", DataFrame)
+    dfR = CSV.read("/global/homes/s/siennaw/scratch/siennaw/scripts/enkf_sediment/lisst_variance.csv", DataFrame)
+
+    observation_times = dfL[!, "seconds"]
+    lisst_data = dfL[!, Not(:seconds)] #./10
+
+    function get_observation_row(time_stamp::Real, df=dfL)
+        # Thank you chatgpt for this function
+        row_idx = findfirst(df.seconds .== time_stamp)
+    
+        # If a match is found, return the row data as a Vector, dropping the seconds column
+        if !isnothing(row_idx)
+            println("Found observation at time = $time_stamp seconds (L_seconds=$(observation_times[row_idx]))")
+            return true, Vector(df[row_idx, Not(:seconds)]).*1e-3
+        else
+            return false, nothing
+        end
+    end
+
+    function get_R(time_stamp::Real, df=dfR)
+        # Thank you chatgpt for this function
+        row_idx = findfirst(df.seconds .== time_stamp)
+        return Vector(df[row_idx, Not(:seconds)])
+       
+    end
+
+
     #***************************************************************************
     #   Initialize variables
     #***************************************************************************
     Times = collect(0:dt:(M*dt))
-
     save_sediment2output(1, ssc0)
     real_times_saved = [Times[1]]
     #***************************************************************************
 
-    # turbulent_shears = zeros(N) .+ 30  
-
     #***************************************************************************
     #   Observations
     #***************************************************************************
-    Ny = 2
-    Y = zeros(Ny) 
+    # Ny = 2
+    # Y = zeros(Ny) 
 
     
-    x_index =10
-    Y[1] = 2000     # floc1
-    Y[2] = 3000     # floc2
+    # x_index =10
+    # Y[1] = 2000     # floc1
+    # Y[2] = 3000     # floc2
 
    
-    H = zeros(Ny, Ns+4)  # + 3 for augmented matrix 
-    H[1, x_index] = 1
-    H[2, x_index+1] = 1
-    println("Inserting observations at size classes $x_index and $(x_index+1)--> Ds=$(D[x_index]*1e6) and Ds=$(D[x_index+1]*1e6) microns")
+    # H = zeros(Ny, Ns+4)  # + 3 for augmented matrix 
+    # H[1, x_index] = 1
+    # H[2, x_index+1] = 1
+    # println("Inserting observations at size classes $x_index and $(x_index+1)--> Ds=$(D[x_index]*1e6) and Ds=$(D[x_index+1]*1e6) microns")
 
 
     # Initialize parameter set for each ensemble member
@@ -141,7 +205,6 @@ function run_my_model(file_out_name::String, floc_on::Bool=true)
         floc_params = init_params(D, Ns, ssc_init, alpha, beta, beta2, nf)
         push!(floc_params_list, floc_params)
         @printf("\tParameters for ensemble [%d]: alpha=%2.2f, beta=%2.2f, beta2=%2.2f, nf=%2.2f\n", i_ens, alpha, beta, beta2, nf)
-
     end
 
     variables = Dict() 
@@ -181,9 +244,12 @@ function run_my_model(file_out_name::String, floc_on::Bool=true)
 
         # ***************************************************************
         # [4] Perform EnKF update  
-        if i%interval ==0 
-            println("Performing EnKF update at time = $time")
-            augmented_matrix = zeros(Ns+4, N) 
+        run_analysis, observations = get_observation_row(time)
+
+        if run_analysis
+            println("Performing EnKF update at time = $(time/3600) hours (timestep $i/$M)")
+            R = get_R(time)
+            augmented_matrix = zeros(Ns_aug, N) 
             for EID in 1:N
                 augmented_matrix[1:Ns, EID] = ssc[EID, :]
                 augmented_matrix[Ns + 1, EID] = Alphas[EID] #+ abs(randn()*0.01)
@@ -193,25 +259,26 @@ function run_my_model(file_out_name::String, floc_on::Bool=true)
             end
 
             ensemble_covariance = cov(augmented_matrix, dims=2) 
-            ensemble_mean = mean(augmented_matrix, dims=2)
-            R = 4e2 #1e-3  # Observation error covariance matrix
-            # R = 4e3 #1e-3 
-
-
-            kalman_gain = (ensemble_covariance * transpose(H)) / (H * ensemble_covariance * transpose(H) + R * I)
+            ensemble_mean = mean(augmented_matrix, dims=2)                                    # construct diag matrix
+            kalman_gain = (ensemble_covariance * transpose(H)) / (H * ensemble_covariance * transpose(H) + diagm(R))
  
             @threads for EID in 1:N
                 # Analysis step 
                 current_state = augmented_matrix[:, EID]
-                innovation = (Y .- H * current_state)
+                innovation = (observations .- H * current_state)
+                # println("Innovation for ensemble [$EID]: ", innovation)
                 shift = kalman_gain * innovation
+                # println("shift for ensemble [$EID]: ", shift)
+
                 augmented_matrix[:, EID] = augmented_matrix[:, EID] + shift
 
                 # Clamp @ zero for no negative concentrations and parameters 
-                augmented_matrix[:, EID] = clamp.(augmented_matrix[:, EID], 1e-5, Inf)
+                augmented_matrix[:, EID] = clamp.(augmented_matrix[:, EID], 1e-12, Inf)
                 
                 # Update sediment concentration for the ensemble 
                 ssc[EID, :] = augmented_matrix[1:Ns, EID]
+                # println("$EID pre-enkf: $(Alphas[EID]), $(Betas[EID]), $(Nfs[EID]), $(Beta2s[EID])")
+                # println("$EID  post-enkf: $(augmented_matrix[Ns:end, EID])")
 
                 # Update parameters for the ensemble
                 Alphas[EID] = clamp(augmented_matrix[Ns + 1, EID], 0.0001, 10.0) 
@@ -225,6 +292,7 @@ function run_my_model(file_out_name::String, floc_on::Bool=true)
                 floc_params_list[EID] = init_params(D, Ns, ssc[EID, :], Alphas[EID], Betas[EID], Beta2s[EID], Nfs[EID])
                 
             end
+            # exit()
         end 
 
         if i % isave == 0
@@ -296,5 +364,5 @@ function run_my_model(file_out_name::String, floc_on::Bool=true)
 
 end 
 
-run_my_model("FlocMod_ADV_G_16.nc", true)
+run_my_model("FlocMod_ADV_G_21.nc", true)
 
